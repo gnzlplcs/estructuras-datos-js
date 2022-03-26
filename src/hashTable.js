69a21779f2d52b2ab063d6f4f4daa18d7e9fb6fd
gnzlplcs <palaciosgm@outlook.com>
2022-03-26 14:28:04 -0500

    🗣️ comments on hashTable file

--- src/hashTable.js
+++ src/hashTable.js
@@ -13,19 +13,31 @@ class HashTable {
 
   set(key, value) {
     const address = this.hashMethod(key);
-    if(!this.data[address]) {
+    if (!this.data[address]) {
       this.data[address] = [];
     }
     this.data[address].push([key, value]);
     return this.data;
   }
+
+  get(key) {
+    const address = this.hashMethod(key); // obtener dirección o index
+    const currentBucket = this.data[address]; // asignamos el bucket actual
+    if (currentBucket) {
+      // si el bucket no está vacío 
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
 
-myHashTable.set('Gonzalo', 1983);
-console.log(myHashTable);
-myHashTable.set('Carmela', 1983);
-console.log(myHashTable);
-myHashTable.set('Alejandra', 1983);
-console.log(myHashTable);
+myHashTable.set("Gonzalo", 1983);
+myHashTable.set("Patricia", 1982);
+myHashTable.set("Lía", 2009);
+console.log(myHashTable.get('Gonzalo'))
